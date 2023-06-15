20c1c5942ddc6c590aee6a059e93faa61ad8e6a9
Abdul Rahim <93329242+developer-rahim@users.noreply.github.com>
2023-06-16 00:57:16 +0600

    fix : Upgrade project 3.10.3 from 3.0.3.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\flutter app test\sms_to_email"
+export "FLUTTER_ROOT=E:\src\versions\3.10.3"
+export "FLUTTER_APPLICATION_PATH=E:\sms-to-email"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
