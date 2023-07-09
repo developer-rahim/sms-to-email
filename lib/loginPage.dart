112ccf38b2b4fd3866197fed9864b976a36253c7
Abdul Rahim <113752743+nextive-rahim@users.noreply.github.com>
2023-07-10 01:06:10 +0600

    feat : Added sniptted loding indicator.

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sms_forward/app_retain_widget.dart';
@@ -132,17 +133,16 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Loading........',
-          style: TextStyle(
-            fontSize: 17,
-            fontWeight: FontWeight.bold,
-            color: Colors.blue,
-          ),
-        ),
+    return Scaffold(body: Center(
+      child: SpinKitFadingCircle(
+        itemBuilder: (BuildContext context, int index) {
+          return DecoratedBox(
+            decoration: BoxDecoration(
+              color: index.isEven ? Colors.red : Colors.green,
+            ),
+          );
+        },
       ),
-    );
+    ));
   }
 }
